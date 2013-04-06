70cf5896cb844985a5a86596848bffe5d4254b82
Mike Foley <mike@signaturelabsinc.com>
2013-04-05 21:05:04 -0700

    add Comment class

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment
+  attr_reader :text
+  def initialize(text)
+    self.text = text
+  end
+end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Comment do
+  it "passes" do
+    pending
+  end
+end
