7f31e12ecdcacf3b44bca952f2676b83f462a0f5
Mike Foley <mike@signaturelabsinc.com>
2013-04-05 18:53:00 -0700

    initial commit, starting over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iriequote::Application.config.secret_token = '127bf9aea2a9bf793cc5dda901e9addd203a199b1e8642c38b0564b200c631651f9931576746ae2d4ec86bdbf4dd539ac08d8f49b653cf0a5e9288727676102e'
